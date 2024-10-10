a65f4efd38cc053ccee4a0c3a02cd38f0a47448d
Alexis <ansonybrine95@gmail.com>
2024-10-10 16:12:32 -0500

    cubas

--- src/main/java/com/mendoza/facturacion/almacen/config/ErrorHandlerConfig.java
+++ src/main/java/com/mendoza/facturacion/almacen/config/ErrorHandlerConfig.java
@@ -1,7 +1,40 @@
 package com.mendoza.facturacion.almacen.config;
 
+import com.mendoza.facturacion.almacen.exception.GeneralException;
+import com.mendoza.facturacion.almacen.exception.NoDataFoundException;
+import com.mendoza.facturacion.almacen.exception.ValidateException;
+import com.mendoza.facturacion.almacen.util.WrapperResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ErrorHandlerConfig extends DefaultErrorHandler {
+public class ErrorHandlerConfig extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> all(Exception e, WebRequest request) {
+        WrapperResponse<?> response = new WrapperResponse<>(false, "Internal server error", null);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ValidateException.class)
+    public ResponseEntity<?> validation(ValidateException e, WebRequest request) {
+        WrapperResponse<?> response = new WrapperResponse<>(false, e.getMessage(), null);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoDataFoundException.class)
+    public ResponseEntity<?> noData(NoDataFoundException e, WebRequest request) {
+        WrapperResponse<?> response = new WrapperResponse<>(false, e.getMessage(), null);
+        return new ResponseEntity<>(response, HttpStatus.NO_CONTENT);
+    }
+
+    @ExceptionHandler(GeneralException.class)
+    public ResponseEntity<?> noData(GeneralException e, WebRequest request) {
+        WrapperResponse<?> response = new WrapperResponse<>(false, "Internal server error", null);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
--- src/main/java/com/mendoza/facturacion/almacen/rest/CategoriaController.java
+++ src/main/java/com/mendoza/facturacion/almacen/rest/CategoriaController.java
@@ -4,9 +4,11 @@ import com.mendoza.facturacion.almacen.converter.CategoriaConverter;
 import com.mendoza.facturacion.almacen.dto.CategoriaDto;
 import com.mendoza.facturacion.almacen.entity.Categoria;
 import com.mendoza.facturacion.almacen.service.CategoriaService;
+import com.mendoza.facturacion.almacen.util.WrapperResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,33 +30,38 @@ public class CategoriaController {
     ) {
         Pageable page = PageRequest.of(pageNumber, pageSize);
         List<CategoriaDto> categorias = converter.fromEntities(service.findAll(page));
-        return ResponseEntity.ok(categorias);
+//        return ResponseEntity.ok(categorias);
+        return new WrapperResponse(true, "success", categorias).createResponse(HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<CategoriaDto> create (@RequestBody CategoriaDto categoria) {
         Categoria entity = converter.fromDTO(categoria);
         CategoriaDto dto = converter.fromEntity(service.save(entity));
-        return ResponseEntity.ok(dto);
+//        return ResponseEntity.ok(dto);
+        return new WrapperResponse(true, "success", dto).createResponse(HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<CategoriaDto> update (@PathVariable("id") int id, @RequestBody CategoriaDto categoria) {
         Categoria entity = converter.fromDTO(categoria);
         CategoriaDto dto = converter.fromEntity(service.save(entity));
-        return ResponseEntity.ok(dto);
+//        return ResponseEntity.ok(dto);
+        return new WrapperResponse(true, "success", dto).createResponse(HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity delete (@PathVariable("id") int id) {
         service.delete(id);
-        return ResponseEntity.ok(null);
+//        return ResponseEntity.ok(null);
+        return new WrapperResponse(true, "success", null).createResponse(HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<CategoriaDto> findById (@PathVariable("id") int id) {
         CategoriaDto dto = converter.fromEntity(service.findById(id));
 
-        return ResponseEntity.ok(dto);
+//        return ResponseEntity.ok(dto);
+        return new WrapperResponse(true, "success", dto).createResponse(HttpStatus.OK);
     }
 }
